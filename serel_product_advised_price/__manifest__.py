d23130f71a278007e2dcf18b875e89f27cae9c5b
rra <rra@idealisconsulting.com>
2020-04-10 16:45:52 +0200

    Delete dependencies (stock & mrp) + deletion of security rules

--- serel_product_advised_price/__manifest__.py
+++ serel_product_advised_price/__manifest__.py
@@ -7,7 +7,7 @@
     'summary': 'Add fields in the Product model',
     'author': 'Idealis Consulting',
     'website': 'http://www.idealisconsulting.com',
-    'depends': ['base', 'product', 'sale', 'stock', 'mrp'],
+    'depends': ['base', 'product', 'sale'],
     'price': 0,
     'currency': 'EUR',
     'data': [
