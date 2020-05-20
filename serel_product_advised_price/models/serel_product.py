64d58c1f2e629e060031620e079b036596a6603c
rra <rra@idealisconsulting.com>
2020-05-20 13:06:12 +0200

    [FIX] Rework on SER-25 : Mandatory field Reference

--- serel_product_advised_price/models/serel_product.py
+++ serel_product_advised_price/models/serel_product.py
@@ -1,19 +1,62 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
 from odoo import models, fields, api, _
+
+_logger = logging.getLogger(__name__)
 
 
 class SerelProductTmpl(models.Model):
     _inherit = 'product.template'
 
-    tag_product_ids = fields.Many2many('tag.product.template', 'product_template_tag_rel', 'product_id', 'tag_id', string='Tags')
-    advised_sale_price = fields.Float(string='Advised Sale Price', digits='Product Price', compute='get_advised_price', store=True)
+    tag_product_ids = fields.Many2many('tag.product.template', 'product_template_tag_rel', 'product_id', 'tag_id',
+                                       string='Tags')
+    advised_sale_price = fields.Float(string='Advised Sale Price', digits='Product Price', compute='get_advised_price',
+                                      store=True)
     default_code = fields.Char(required=True)
 
     _sql_constraints = [
         ('unique_default_code', 'unique(default_code)', 'This reference already exists!')
     ]
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        ''' Store the initial standard price in order to be able to retrieve the cost of a product template for a given date'''
+        templates = super(SerelProductTmpl, self).create(vals_list)
+        if "create_product_product" not in self._context:
+            templates._create_variant_ids()
+
+        # This is needed to set given values to first variant after creation
+        for template, vals in zip(templates, vals_list):
+            related_vals = {}
+            if vals.get('barcode'):
+                related_vals['barcode'] = vals['barcode']
+            # if vals.get('default_code'):
+            #     related_vals['default_code'] = vals['default_code']
+            if vals.get('standard_price'):
+                related_vals['standard_price'] = vals['standard_price']
+            if vals.get('volume'):
+                related_vals['volume'] = vals['volume']
+            if vals.get('weight'):
+                related_vals['weight'] = vals['weight']
+            # Please do forward port
+            if vals.get('packaging_ids'):
+                related_vals['packaging_ids'] = vals['packaging_ids']
+            if related_vals:
+                template.write(related_vals)
+
+        return templates
+
+    @api.returns('self', lambda value: value.id)
+    def copy(self, default=None):
+        self.ensure_one()
+        if default is None:
+            default = {}
+        if 'default_code' not in default:
+            default['default_code'] = self.default_code + self.env['ir.sequence'].next_by_code('product.product')
+        return super(SerelProductTmpl, self).copy(default=default)
+    
     @api.depends('standard_price')
     def get_advised_price(self):
         """
@@ -28,31 +71,74 @@ class SerelProductTmpl(models.Model):
             else:
                 product_tmpl.advised_sale_price = 0
 
+    def _create_product_variant(self, combination, log_warning=False):
+        """ Create if necessary and possible and return the product variant
+        matching the given combination for this template.
+
+        It is possible to create only if the template has dynamic attributes
+        and the combination itself is possible.
+        If we are in this case and the variant already exists but it is
+        archived, it is activated instead of being created again.
+
+        :param combination: the combination for which to get or create variant.
+            The combination must contain all necessary attributes, including
+            those of type no_variant. Indeed even though those attributes won't
+            be included in the variant if newly created, they are needed when
+            checking if the combination is possible.
+        :type combination: recordset of `product.template.attribute.value`
+
+        :param log_warning: whether a warning should be logged on fail
+        :type log_warning: bool
+
+        :return: the product variant matching the combination or none
+        :rtype: recordset of `product.product`
+        """
+        self.ensure_one()
+
+        Product = self.env['product.product']
+
+        product_variant = self._get_variant_for_combination(combination)
+        if product_variant:
+            if not product_variant.active and self.has_dynamic_attributes() and self._is_combination_possible(combination):
+                product_variant.active = True
+            return product_variant
+
+        if not self.has_dynamic_attributes():
+            if log_warning:
+                _logger.warning('The user #%s tried to create a variant for the non-dynamic product %s.' % (self.env.user.id, self.id))
+            return Product
+
+        if not self._is_combination_possible(combination):
+            if log_warning:
+                _logger.warning('The user #%s tried to create an invalid variant for the product %s.' % (self.env.user.id, self.id))
+            return Product
+
+        return Product.sudo().create({
+            'product_tmpl_id': self.id,
+            'product_template_attribute_value_ids': [(6, 0, combination._without_no_variant_attributes().ids),],
+            'default_code': self.default_code + self.env['ir.sequence'].next_by_code('product.product')
+        })
+
+    @api.depends('product_variant_ids', 'product_variant_ids.default_code')
+    def _compute_default_code(self):
+        return False
+
+    def _set_default_code(self):
+        return False
+
 
 class SerelProductProduct(models.Model):
     _inherit = 'product.product'
 
-    tag_product_ids = fields.Many2many('tag.product.template', 'product_product_tag_rel', 'product_id', 'tag_id', related="product_tmpl_id.tag_product_ids", string='Tags', readonly=True)
-    pr_advised_sale_price = fields.Float(string='Pr Advised Sale Price', digits='Product Price', compute='get_advised_price', store=True)
+    tag_product_ids = fields.Many2many('tag.product.template', 'product_product_tag_rel', 'product_id', 'tag_id',
+                                       related="product_tmpl_id.tag_product_ids", string='Tags', readonly=True)
+    pr_advised_sale_price = fields.Float(string='Pr Advised Sale Price', digits='Product Price',
+                                         compute='get_advised_price', store=True)
     default_code = fields.Char(required=True)
 
     _sql_constraints = [
         ('unique_default_code', 'unique(default_code)', 'This reference already exists!')
     ]
-
-    @api.model
-    def create(self, vals):
-        # self.ensure_one()
-        product_tmpl_configurator = self.env['product.template'].search([('product_add_mode', '=', 'configurator'),
-                                                                         ('id', '=', vals['product_tmpl_id'])])
-        if vals['product_tmpl_id'] in product_tmpl_configurator.ids:
-            ref = product_tmpl_configurator.default_code
-            vals['default_code'] = ref + self.env['ir.sequence'].next_by_code('product.product')
-            # res.default_code = ref + self.env['ir.sequence'].next_by_code('product.product')
-            product_tmpl_configurator.default_code = ref
-        # else:
-            # res = super(SerelProductProduct, self).create(vals)
-        return super(SerelProductProduct, self).create(vals)
 
     @api.depends('standard_price')
     def get_advised_price(self):
