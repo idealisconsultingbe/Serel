8256d9eb6384e1a97209cba9a387909fa041f0f1
rra <rra@idealisconsulting.com>
2020-07-17 14:17:48 +0200

    [ADD] SER-45 : Tous les points sauf logique section

--- serel_product_advised_price/models/__init__.py
+++ serel_product_advised_price/models/__init__.py
@@ -3,3 +3,5 @@
 from . import serel_product
 from . import serel_product_pricelist
 from . import serel_sale_order
+from . import serel_res_partner
+from . import serel_account_move
--- serel_product_advised_price/models/serel_sale_order.py
+++ serel_product_advised_price/models/serel_sale_order.py
@@ -3,12 +3,14 @@
 from odoo import models, fields, api, _
 
 
-class SerelSaelOrderLine(models.Model):
+class SerelSaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     advised_sale_price = fields.Float('Advised Sale Price', digits='Product Price',
                                       related='product_id.pr_advised_sale_price')
     show_in_report = fields.Boolean(string='Show in Report', default=True)
+    show_in_report_section = fields.Boolean(string='Show in Report Section')
+
     qty_integer = fields.Integer(string='Quantity in Integer', compute='_compute_qty_integer', store=True)
 
     @api.depends('product_uom_qty')
@@ -17,3 +19,12 @@ class SerelSaelOrderLine(models.Model):
             line.qty_integer = int(line.product_uom_qty)
             if line.qty_integer != line.product_uom_qty:
                 line.qty_integer = False
+
+    @api.onchange('show_in_report')
+    def _onchange_show_in_report_section(self):
+        for line in self:
+            for so_line in line.order_id.order_line:
+                if so_line.display_type == 'line_section' and so_line.display_name == line.display_name:
+                    line.show_in_report_section = so_line.show_in_report
+                else:
+                    line.show_in_report_section = True  # TO CHANGE
